2964350ba579fc1068ffb111e532abf41c888058
Eduardo Monteiro <dudumonteiro@gmail.com>
2018-06-04 13:02:41 -0300

    Incluindo app.py e config.py e melhorias em README.md

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 from flask import Flask, jsonify, redirect
 from flask_pymongo import PyMongo
 
+from config import *
+
+
 app = Flask('du2x')
 mongo = PyMongo(app)
 
 
 @app.route('/')
 def index():
-    users = mongo.db.users.find({})
-    return jsonify(list(users))
+    res = mongo.db.stuff.find({})
+    return jsonify(list(res))
 
 @app.route('/create/')
 def create(mongodb):
-    mongo.db.users.insert({'a': 1, 'b': 2})
+    mongo.db.stuff.insert({'a': 1, 'b': 2})
     redirect("/")
 
-
-app.run()
+app.run(host='0.0.0.0', debug=True)
